b82e72c0a17a8482ce242fd27c391620e000ca95
Mukti Wibowo <muktiwbowo@gmail.com>
2021-12-23 09:36:40 +0700

    Initial Commit

---
+++ WarCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WarCard
+//
+//  Created by Mukti Wibowo on 23/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WarCard/WarCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardApp.swift
+//  WarCard
+//
+//  Created by Mukti Wibowo on 23/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WarCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
